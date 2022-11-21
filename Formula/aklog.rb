14023089c8ad42c2acb634f02114bf31f8d9be7d
CodingTheWorld <wswenyue@users.noreply.github.com>
2022-11-21 10:41:33 +0800

    V4.0.0.0

--- Formula/aklog.rb
+++ Formula/aklog.rb
@@ -4,8 +4,8 @@
 class Aklog < Formula
   desc "Android developer's Swiss Army Knife for Log"
   homepage "https://github.com/wswenyue/aklog"
-  url "https://github.com/wswenyue/aklog/archive/v3.0.0.tar.gz"
-  sha256 "92baa90584556aaf6eadf85a6a193f2226b0351d70ed833efa7ea83a7827176d"
+  url "https://github.com/wswenyue/aklog/archive/v4.0.0.tar.gz"
+  sha256 "4ce51e099aa3a7d93eca6764b878a8a0d2f9fd06d7981af8f15690d181f58574"
   
 
   def install
