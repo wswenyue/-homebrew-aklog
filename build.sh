123c42ffbf03c4cf442ba9d59b7773d601a3da14
wswenyue <wswenyue@163.com>
2024-03-15 15:20:26 +0800

    remove

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Author: wswenyue.
-#Date & Time: 2023-08-25 17:56:38
-#Description: a bash script
-wget -O aklog.tar.gz https://github.com/wswenyue/aklog/archive/v5.0.4.tar.gz \
-&& openssl dgst -sha256 aklog.tar.gz && rm -rf ./aklog.tar.gz
